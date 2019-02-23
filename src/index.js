f65def66139ba4d839533fc925a02e3637aa570e
kaspaamulya <47910786+kaspaamulya@users.noreply.github.com>
2019-02-23 12:09:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function showPassword() {
+  var key_attr = $("#key").attr("type");
+
+  if (key_attr != "text") {
+    $(".checkbox").addClass("show");
+    $("#key").attr("type", "text");
+  } else {
+    $(".checkbox").removeClass("show");
+    $("#key").attr("type", "password");
+  }
+}
